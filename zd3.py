60d0d173ca85fc0f3ebf7fc20577e50e137fcb8d
Maria-28 <80480943+Maria-28@users.noreply.github.com>
2021-12-22 12:46:56 +0300

    Update zd3.py

--- zd3.py
+++ zd3.py
@@ -8,6 +8,8 @@ def intro(**data):
 
     for key, value in data.items():
         print("{} is {}".format(key, value))
+ 
 
+if __name__ == '__main__':
 intro(Firstname="МАША", Lastname="СЕРИКОВА", Age=19, Phone=18938377890)
 intro(Firstname="РАФИК", Lastname="ПИКАЧУ", Email="nhdhhdhdh@nomail.com", Country="Russia", Age=19, Phone=9876543210)
