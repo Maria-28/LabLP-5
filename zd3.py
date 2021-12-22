d33c556fc67be8bf6ecb19f4b4d68777c28f7360
Maria-28 <80480943+Maria-28@users.noreply.github.com>
2021-12-22 11:58:42 +0300

    Add files via upload

---
+++ zd3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def intro(**data):
+    print("\nData type of argument: ",type(data))
+
+    for key, value in data.items():
+        print("{} is {}".format(key, value))
+
+intro(Firstname="МАША", Lastname="СЕРИКОВА", Age=19, Phone=18938377890)
+intro(Firstname="РАФИК", Lastname="ПИКАЧУ", Email="nhdhhdhdh@nomail.com", Country="Russia", Age=19, Phone=9876543210)
